3490e2e5da528b34e28b92bcee360d4d00796647
Matyas Korytar <matyas.korytar.021@pslib.cz>
2024-05-06 15:44:09 +0200

    Commit

--- Kurnik/src/App.tsx
+++ Kurnik/src/App.tsx
@@ -1,25 +1,24 @@
-import { GameProvider } from './providers/GameContext'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import PlayerScreen from './components/PlayerScreen'
-import EnemyScreen from './components/EnemyScreen'
-import EnemyResultScreen from './components/EnemyResultScreen'
-import PlayerResultScreen from './components/PlayerResultScreen'
+import { GameProvider } from './providers/GameContext';
+import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import PlayerScreen from './components/PlayerScreen';
+import EnemyScreen from './components/EnemyScreen';
+import EnemyResultScreen from './components/EnemyResultScreen';
+import PlayerResultScreen from './components/PlayerResultScreen';
 
 function App() {
-  
   return (
     <Router>
       <GameProvider>
         <Routes>
-            <Route path="/" element={<PlayerScreen />} />
-            <Route path="/player-result" element={<PlayerResultScreen />} />
-            <Route path="/enemy-turn" element={<EnemyScreen />} />
-            <Route path="/enemy-result" element={<EnemyResultScreen />} />
+          <Route path="/" element={<PlayerScreen />} />
+          <Route path="/player-result" element={<PlayerResultScreen />} />
+          <Route path="/enemy-turn" element={<EnemyScreen />} />
+          <Route path="/enemy-result" element={<EnemyResultScreen />} />
         </Routes>
       </GameProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- Kurnik/vite.config.ts
+++ Kurnik/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2023-p3a-mpa-react-project-MatyasKorytar/',
   plugins: [react()],
   build: {
     outDir: 'build'
